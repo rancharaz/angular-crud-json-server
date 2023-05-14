7f5af8da4634988a050f879dcde6fb2bfc4e92ce
rancharaz <ranveer1990@hotmail.com>
2023-05-14 23:35:40 +0400

    appear menu on seller logic in header

--- frontendAngular/src/app/header/header.component.ts
+++ frontendAngular/src/app/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+
 
 @Component({
   selector: 'app-header',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+/* for condition and can be use in html */
+  menuType:String = 'default';
 
-  constructor() { }
+  /* intantiate router module */
+  constructor(private route:Router) { }
 
   ngOnInit(): void {
+    /* getting the href path */
+    this.route.events.subscribe((value:any) => {
+      if(value.url){
+        console.warn(value.url);
+        /* if login value and contain seller in href*/
+        if (localStorage.getItem('seller') && value.url.includes('seller')) {
+          console.warn("This is seller area");
+          this.menuType = "seller"
+        } else {
+          console.warn("Outside seller area");
+          this.menuType = "default";
+        }
+      }
+
+    })
   }
 
 }
